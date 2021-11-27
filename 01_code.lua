1a698e68c3776c297ccb071a77f23704ee4ce122
Andrei Ehrlich <andrei.ehrlich@gmail.com>
2021-11-27 13:12:09 +0100

    provide solution for day 1 (part 2).
    Signed-off-by: Andrei Ehrlich <andrei.ehrlich@gmail.com>

--- 01_code.lua
+++ 01_code.lua
@@ -42,16 +42,23 @@ local reader  = Reader:new()
 local numbers = reader(input)
 local desired = 2020
 
-while(#numbers > 1) do
+while(#numbers > 2) do
   print("Remaining ... " .. #numbers)
-  local head = table.remove(numbers, 1)
-  for _, v in ipairs(numbers) do
-    local sum = head + v
-    if(desired == sum) then
-      local mul = head * v
-      print(sum .. " = " .. head .. " + " .. v)
-      print(mul .. " = " .. head .. " * " .. v)
-      return
+  local base = table.remove(numbers, 1) -- base contains the first table element (that will be dropped in the ext iteration)
+  local size = #numbers
+  local key = 1
+  while(key + 2 < size) do
+    local idx = key + 1
+    while(idx + 1 < size) do
+      local sum = base + numbers[key] + numbers[idx]
+      if(desired == sum) then
+        local mul = base * numbers[key] * numbers[idx]
+        print(sum .. " = " .. base .. " + " .. numbers[key] .. " + " .. numbers[idx])
+        print(mul .. " = " .. base .. " * " .. numbers[key] .. " + " .. numbers[idx])
+        return
+      end
+      idx = idx + 1
     end
+    key = key + 1
   end
 end
