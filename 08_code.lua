78cc574fddf61f578009b92d5b32e9b60f079f14
Andrei Ehrlich <andrei.ehrlich@gmail.com>
2021-12-26 22:16:44 +0100

    provide solution for day 8 (part 2).
    Signed-off-by: Andrei Ehrlich <andrei.ehrlich@gmail.com>

--- 08_code.lua
+++ 08_code.lua
@@ -4,27 +4,49 @@
 local reader = require("reader"):new()
 local commands = reader("./08_data.txt")
 local accumulator = 0
-local executed_commands = {}
 
-local idx = 1
-while idx <= #commands do
-  local line = tostring(commands[idx])
-  local operation = line:match("%a+")
-  local argument = tonumber(line:match("[+-]%d+"), 10)
+local execution = function(idx2correct)
+  accumulator = 0
+  local idx = 1
+  local executed_commands = {}
 
-  for _, v in pairs(executed_commands) do
-    if idx == v then
-      print('~> Infinite loop detected!')
-      print('~> Current command index is ' .. idx)
-      print('~> Current accumulator value is ' .. accumulator)
-      return
+  while idx <= #commands do
+    local line = tostring(commands[idx])
+    local operation = line:match("%a+")
+    local argument = tonumber(line:match("[+-]%d+"), 10)
+
+    for _, v in pairs(executed_commands) do
+      if idx == v.idx then
+        print('~> Infinite loop detected!')
+        print('~> Current command index is ' .. idx)
+        print('~> Current accumulator value is ' .. accumulator)
+        return false
+      end
     end
-  end
-  table.insert(executed_commands, idx)
+    table.insert(executed_commands, {idx = idx, cmd = line})
+    if idx == idx2correct then
+      if('jmp' == operation) then
+        print('~> Changing command [' .. idx .. '] ' .. line)
+        operation = 'nop'
+      elseif 'nop' == operation then
+        print('~> Changing command [' .. idx .. '] ' .. line)
+        operation = 'jmp'
+      end
+    end
 
-  if(operation == 'acc') then
-    accumulator = accumulator + argument
-  end
+    if(operation == 'acc') then
+      accumulator = accumulator + argument
+    end
 
-  idx = idx + ((operation == 'jmp') and argument or 1)
+    idx = idx + ((operation == 'jmp') and argument or 1)
+  end
+  return true
+end
+
+for i = 1, #commands, 1 do
+  if execution(i) then
+    print('~> Changing command [' .. i .. '] fixed infinite loop!')
+    print('~> Current accumulator value is ' .. accumulator)
+    return
+  end
 end
